d97e5a5b401472466d3aca869de21540d2cf1329
Andreas Kreutz <andreas.kreutz@hotmail.de>
2016-05-03 15:49:55 +0200

    Added springs and linear momentum
    -Changed the order of finding and resolving collision so that impulses
    can be applied once per frame.
    
    -Added a Scene 21 to cScenes for isolated testing of momentum

--- src/sbndengine/physics/cPhysicsEngine_Private.cpp
+++ src/sbndengine/physics/cPhysicsEngine_Private.cpp
@@ -304,7 +304,7 @@ bool cPhysicsEngine_Private::simulationTimestep(double p_elapsed_time)
 #endif
 
 #if WORKSHEET_3
-
+	updateSoftConstraints();
 #endif
 
 #if WORKSHEET_1
@@ -312,11 +312,11 @@ bool cPhysicsEngine_Private::simulationTimestep(double p_elapsed_time)
 #endif
 
 #if WORKSHEET_2
-
+	emptyAndGetCollisions();
 #endif
 
 #if WORKSHEET_3
-
+	applyCollisionImpulse();
 #endif
 
 #if WORKSHEET_3
@@ -326,11 +326,10 @@ bool cPhysicsEngine_Private::simulationTimestep(double p_elapsed_time)
 
 	int i = 1;
 #if WORKSHEET_2
-	for (; i < max_global_collision_solving_iterations; ++i) {
-		emptyAndGetCollisions();
-		if (list_colliding_objects.empty())
-			break;
+	while (!list_colliding_objects.empty() && i < max_global_collision_solving_iterations) {
 		resolveInterpenetrations();
+		emptyAndGetCollisions();
+		i++;
 	}
 #endif
 
