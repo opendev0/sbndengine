d0851abe4b55e56f37cdf531849e4130a893a9a7
opendev0 <opendev0@gmail.com>
2016-07-08 09:43:04 +0200

    Added a small level to the game

---
+++ src/game/RandomPathEnemy.hpp
@@ -0,0 +1,16 @@
+#ifndef RANDOMPATHENEMY_HPP
+#define RANDOMPATHENEMY_HPP
+
+class RandomPathEnemy
+{
+public:
+	RandomPathEnemy()
+	{
+	}
+	~RandomPathEnemy()
+	{
+	}
+
+};
+
+#endif // RANDOMPATHENEMY_HPP
